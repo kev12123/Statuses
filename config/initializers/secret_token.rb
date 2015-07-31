9f1a850caf55cab0e0245cc3c28ba776dc428247
kev12123 <kevin_12123@hotmail.com>
2015-07-31 03:39:22 -0400

    Rails application with Status update

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MutinyBook::Application.config.secret_token = '38772677ee8d220923a3e7daf105c9d882f82df3a4f26248cb76ac6bdac2e29583ef570a8eee4403d845d2e6e822edb6ee322ff6fe594af98f24c256df648153'
